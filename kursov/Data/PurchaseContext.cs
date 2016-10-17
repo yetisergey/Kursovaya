b04de1bb532a0f676214fe4743c241d7794012d6
Сергей <yetisergey@gmail.com>
2016-10-17 19:45:37 +0300

    Начал делать базу

---
+++ kursov/Data/PurchaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace Data
+{
+    class PurchaseContext:DbContext
+    {
+        public PurchaseContext()
+            :base("DbConnection")
+        { }
+
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
