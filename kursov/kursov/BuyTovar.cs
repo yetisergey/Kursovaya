6754486bff0e84b366a2f2705450476aa1d35128
Сергей Масленников <yetisergey@gmail.com>
2016-10-17 17:27:20 +0300

    первый коммит курсач

---
+++ kursov/kursov/BuyTovar.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace kursov
+{
+    public partial class BuyTovar : Form
+    {
+        private int Price;
+        private double CursDol;
+        class HelperSelected
+        {
+            public string name { get; set; }
+            public int price { get; set; }
+        }
+        public BuyTovar(string tempname)
+        {
+            InitializeComponent();
+            label1.Text = tempname;
+            CursDol = GET("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+            if (tempname == "Бетон")
+            {
+                listBox1.DisplayMember = "name";
+                listBox1.ValueMember = "price";
+                listBox1.Items.Add(new HelperSelected { name = "2 вид 200", price = 200 });
+                listBox1.Items.Add(new HelperSelected { name = "1 вид 100", price = 100 });
+                listBox1.SelectedIndex = 0;
+            }
+            if (tempname == "Керамзитобетон")
+            {
+                listBox1.DisplayMember = "name";
+                listBox1.ValueMember = "price";
+                listBox1.Items.Add(new HelperSelected { name = "user1", price = 100 });
+                listBox1.SelectedIndex = 0;
+            }
+            if (tempname == "Асфальтобетон")
+            {
+                listBox1.DisplayMember = "name";
+                listBox1.ValueMember = "price";
+                listBox1.Items.Add(new HelperSelected { name = "user1", price = 100 });
+                listBox1.SelectedIndex = 0;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Count.Text = (int.Parse(Count.Text) + 1).ToString();
+            label9.Text = (int.Parse(Count.Text) * Price).ToString() + " руб";
+            label12.Text = (int.Parse(Count.Text) * Price / CursDol).ToString() + "$";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(Count.Text) > 1)
+            {
+                Count.Text = (int.Parse(Count.Text) - 1).ToString();
+                label9.Text = (int.Parse(Count.Text) * Price).ToString() + " руб";
+                label12.Text =(int.Parse(Count.Text) * Price /  Math.Round(CursDol, 2)).ToString() + "$";
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HelperSelected temp = (HelperSelected)listBox1.SelectedItem;
+            Count.Text = "0";
+            label9.Text = "0";
+            label12.Text = "0";
+            Price = temp.price;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private static double GET(string Url)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(Url);
+            System.Net.WebResponse resp = req.GetResponse();
+            System.IO.Stream stream = resp.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(stream);
+            string xmlString = sr.ReadToEnd();
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmlString);
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xml.NameTable);
+            nsmgr.AddNamespace("ecb", "http://www.ecb.int/vocabulary/2002-08-01/eurofxref");
+            nsmgr.AddNamespace("gesmes", "http://www.gesmes.org/xml/2002-08-01");
+            XmlNode currencyNode = xml.SelectSingleNode("descendant::ecb:Cube[@currency='RUB']", nsmgr);
+            string rate = currencyNode.Attributes.GetNamedItem("rate").Value.Trim().Replace(".",",");
+            float drate = float.Parse(rate);
+            sr.Close();
+            return drate;
+        }
+    }
+}
---
+++ kursov/kursov/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace kursov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linklabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.linkLabel6 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::kursov.Properties.Resources.Beton;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 160);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::kursov.Properties.Resources.asfbeton;
+            this.pictureBox2.Location = new System.Drawing.Point(282, 52);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(160, 160);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Felix Titling", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(176, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 32);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Товары Бетонного завода";
+            // 
+            // linklabel1
+            // 
+            this.linklabel1.AutoSize = true;
+            this.linklabel1.Font = new System.Drawing.Font("Felix Titling", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linklabel1.Location = new System.Drawing.Point(94, 215);
+            this.linklabel1.Name = "linklabel1";
+            this.linklabel1.Size = new System.Drawing.Size(57, 24);
+            this.linklabel1.TabIndex = 13;
+            this.linklabel1.TabStop = true;
+            this.linklabel1.Text = "Бетон";
+            this.linklabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linklabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Felix Titling", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.Location = new System.Drawing.Point(294, 215);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(135, 24);
+            this.linkLabel2.TabIndex = 14;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Асфальтобетон";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::kursov.Properties.Resources.keramzit;
+            this.pictureBox3.Location = new System.Drawing.Point(496, 52);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(160, 160);
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Font = new System.Drawing.Font("Felix Titling", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel3.Location = new System.Drawing.Point(509, 215);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(138, 24);
+            this.linkLabel3.TabIndex = 16;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Керамзитобетон";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(45, 280);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(160, 160);
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Font = new System.Drawing.Font("Felix Titling", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel4.Location = new System.Drawing.Point(12, 443);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(218, 24);
+            this.linkLabel4.TabIndex = 18;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "Аренда Автобетононасоса";
+            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(282, 280);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(160, 160);
+            this.pictureBox5.TabIndex = 19;
+            this.pictureBox5.TabStop = false;
+            // 
+            // linkLabel5
+            // 
+            this.linkLabel5.AutoSize = true;
+            this.linkLabel5.Font = new System.Drawing.Font("Felix Titling", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel5.Location = new System.Drawing.Point(278, 443);
+            this.linkLabel5.Name = "linkLabel5";
+            this.linkLabel5.Size = new System.Drawing.Size(177, 24);
+            this.linkLabel5.TabIndex = 20;
+            this.linkLabel5.TabStop = true;
+            this.linkLabel5.Text = "Аренда Цементовоза";
+            this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(496, 280);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(160, 160);
+            this.pictureBox6.TabIndex = 21;
+            this.pictureBox6.TabStop = false;
+            // 
+            // linkLabel6
+            // 
+            this.linkLabel6.AutoSize = true;
+            this.linkLabel6.Font = new System.Drawing.Font("Felix Titling", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel6.Location = new System.Drawing.Point(500, 443);
+            this.linkLabel6.Name = "linkLabel6";
+            this.linkLabel6.Size = new System.Drawing.Size(156, 24);
+            this.linkLabel6.TabIndex = 22;
+            this.linkLabel6.TabStop = true;
+            this.linkLabel6.Text = "Аренда Автокрана";
+            this.linkLabel6.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel6_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 497);
+            this.Controls.Add(this.linkLabel6);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.linkLabel5);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.linkLabel4);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linklabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linklabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.LinkLabel linkLabel5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.LinkLabel linkLabel6;
+    }
+}
+
