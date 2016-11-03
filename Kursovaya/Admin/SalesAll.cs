ee2fe43ada25feb2960a2f2eef8a4b6b1d12709d
Сергей Масленников <yetisergey@gmail.com>
2016-11-03 21:45:13 +0300

    merge

---
+++ Kursovaya/Admin/SalesAll.cs
@@ -0,0 +1,29 @@
+﻿using Data;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Admin
+{
+    public partial class SalesAll : Form
+    {
+        public SalesAll()
+        {
+            InitializeComponent();
+            using (BaseContext db = new BaseContext())
+            {
+                dataGridView1.DataSource = db.Purchases.Select(u => new {
+                    //Id = u.Id.ToString(),
+                    FIO = u.Fio.ToString(),
+                    PasportId = u.PasportId.ToString(),
+                    Telephone = u.Telephone.ToString(),
+                    Price = u.Price.ToString(),
+                    CountTovar = u.Counttovar.ToString(),
+                    Date = u.Date,
+                    //.ToString("dd.MM.yyyy"),
+                    Prod = u.prod.Name.ToString()
+                }).ToList();
+            }
+        }
+    }
+}
